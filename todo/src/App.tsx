15e146019048381f7eaf61da1d425cb9f2047759
Evandro oliveira <vandoaparecido@hotmail.com>
2021-10-04 20:38:44 -0300

    :bookmark: Criando lista estática

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import * as C from "./App.styles";
+import { Item } from "./types/Item";
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: "Fazer compras", done: false },
+    { id: 2, name: "Cortar o cabelo", done: true },
+  ]);
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de tarefas</C.Header>
+        {list.map((item, index) => (
+          <div>
+            {item.id} - {item.name}
+          </div>
+        ))}
       </C.Area>
     </C.Container>
   );
