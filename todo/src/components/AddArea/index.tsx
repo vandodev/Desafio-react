6846cf8c0dfd81f3d9f0e1456d1347e442407dc7
Evandro oliveira <vandoaparecido@hotmail.com>
2021-10-04 21:58:45 -0300

    :bookmark: Capturando tarefa inserida

--- todo/src/components/AddArea/index.tsx
+++ todo/src/components/AddArea/index.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import * as C from "./styles";
 
 export const AddArea = () => {
+  const [inputText, setInputText] = useState("");
+
   return (
     <C.Container>
       <div className="image">➕</div>
-      <input type="text" placeholder="Adicione uma tarefa" />
+      <input
+        type="text"
+        placeholder="Adicione uma tarefa"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
     </C.Container>
   );
 };
